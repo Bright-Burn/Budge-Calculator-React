d82f9911c1fe529ce6b0af284a6866f4e59a76ac
Ilgiz <ilgiz93sharafutdinov@gmail.com>
2020-02-09 16:34:08 +0500

    Updates

---
+++ precache-manifest.1634b3078c57360873117f5961612e22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fe39738220bd958958c6d4be29c1c7",
+    "url": "/Budge-Calculator-React/index.html"
+  },
+  {
+    "revision": "d08c7232b0e382361a6d",
+    "url": "/Budge-Calculator-React/static/css/main.4713adcf.chunk.css"
+  },
+  {
+    "revision": "b399fcf6b0d100e9cd7c",
+    "url": "/Budge-Calculator-React/static/js/2.1ba98958.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Budge-Calculator-React/static/js/2.1ba98958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08c7232b0e382361a6d",
+    "url": "/Budge-Calculator-React/static/js/main.32602657.chunk.js"
+  },
+  {
+    "revision": "86a99f06162594a91e4e",
+    "url": "/Budge-Calculator-React/static/js/runtime-main.ed06d6f6.js"
+  }
+]);
